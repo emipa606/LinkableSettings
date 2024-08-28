2f24071cec9f2244cfc6197e1b28ccb6b587cafa
Mlie <emipa606@users.noreply.github.com>
2024-08-28 22:13:27 +0200

    Should now work with the new types of facilities

--- Source/LinkableSettings/Main.cs
+++ Source/LinkableSettings/Main.cs
@@ -30,7 +30,8 @@ public static class Main
         var harmony = new Harmony("Mlie.LinkableSettings");
         harmony.PatchAll(Assembly.GetExecutingAssembly());
         AllFacilities = DefDatabase<ThingDef>.AllDefsListForReading
-            .Where(def => def.HasComp(typeof(CompFacility))).ToList();
+            .Where(def => def.HasComp<CompFacility>() || def.HasComp<CompFacilityInactiveWhenElectricityDisabled>() ||
+                          def.HasComp<CompFacilityQualityBased>()).ToList();
         if (LinkableSettingsMod.instance.Settings.FacilityType == null)
         {
             LinkableSettingsMod.instance.Settings.FacilityType = new Dictionary<string, int>();
